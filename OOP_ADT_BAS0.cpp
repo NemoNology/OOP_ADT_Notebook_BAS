a41e219ddced007e4fb0ff68886c7db64f2e43a7
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-12-10 13:57:03 +0900

    V0.7: Добавь использование класса! (Массив из классов, указатели и др.)
    V0.7: Всё нужное указано в наименовании comit'a

--- OOP_ADT_BAS0.cpp
+++ OOP_ADT_BAS0.cpp
@@ -1,126 +1,28 @@
 ﻿#include <iostream>
 #include <exception>
-
-// Я - Александр Банковский Сергеевич.
-// Одно из доказательств - то, что эти файлы лежат в репозитории
-// OOP_ADT_Notebook_BAS в github.com пользователя
-// NemoNology - меня!
-
-
-// todo: add file
+#include <string>
+#include "Notebook.h"
 
 
 using namespace std;
-
-// Создание множество для класса notebook
-
-
-
-// todo: 
-class Notebook {
-
-public: 
-    
-    enum Markup_T {         // Тип разметки
-
-    Checkered,          // В клетку
-    In_Line,            // В линию  
-    Without_Markup,     // Без разметки
-    Another             // Другой
-
-    };
-
-    enum Paper_M {          // Материал бумаги\листов
-
-        EAC,            // ГОСТ
-        Another         // Не ГОСТ
-
-    };
-
-
-private:
-
-    Markup_T Markup_Type;                   // Тип разметки
-    unsigned short Number_of_Sheets;        // Кол-во листов
-    string Manufacturer;                    // Производитель
-    Paper_M Paper_Material;                 // Материал бумаги\листов
-    unsigned short Size_of_Page;                    // Размер страницы (А5, например)   
-
-    Notebook() {                            // Конструктор
-
-        Markup_Type = Markup_T::Without_Markup;
-        Number_of_Sheets = 0;
-        Manufacturer = "Unknown";
-        Paper_Material = Paper_M::EAC;
-        Size_of_Page = 5;
-
-    };
-
-public:
-
-    
-    // Set Тип разметки
-    void Set_Markup_Type(Markup_T t) {
-    
-        this->Markup_Type = t;
-
-    };
-
-    // Set Кол-во листов
-    void Set_Number_of_Sheets(unsigned short n) { 
-        // todo: if
-          // todo: throw
-        
-        this->Number_of_Sheets = n; }
-
-    // Set Производитель
-    void Set_Manufacturer(string m) {     // todo: const &
-        
-        // При вводе ничего, не меняем 
-        if (m == "") { 
-            
-            throw invalid_argument("Пусто...");
-        
-        }
-        else {
-
-            this->Manufacturer = m;
-
-        }
-    }
-
-    // Set Материал бумаги\листов
-    void Set_Paper_material(Paper_M m) {
-
-            this->Paper_Material = m;
-
-    }
-
-    // Set Размер страницы
-    void Set_Size_of_Page(unsigned short s) {
-
-        if (s == 0) {
-
-            throw invalid_argument("Нулевой размер?..");
-
-        }
-        else {
-
-            this->Size_of_Page = s;
-
-        }
-    }
-
-};
 
 
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+  
+
+
+    Notebook test1(Notebook::Markup_T::Without_Markup, 96, "ООО Забайкальский Государственный целюлозно-бумажный завод", Notebook::Paper_M::Not_EAC, 5);
+
+
+    Notebook test2;
     
 
+    cout << test1.Get_Notebook_Information() << endl;
 
-    
 
+    system("pause");
     return 0;
 }
